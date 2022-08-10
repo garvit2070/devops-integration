fa4cef99d0adbede9bcd110cf2dbd43acca9588e
garvit2070 <garvitjain6464@gmail.com>
2022-08-10 17:57:09 +0530

    initial commit

---
+++ src/main/java/com/training/devops/integration/myclass.java
@@ -0,0 +1,19 @@
+package com.training.devops.integration;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author garvi
+ */
+public class myclass {
+    
+        public static void main(String[] args) {
+        // TODO code application logic here
+            System.out.println("Hello World!");
+    }
+    
+}
